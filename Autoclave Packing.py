379e6755afff606211cccdf2826aee71b5abb701
LorenzVeithen <80531117+LorenzVeithen@users.noreply.github.com>
2021-03-26 16:23:45 +0100

    Update Autoclave Packing.py

--- Autoclave Packing.py
+++ Autoclave Packing.py
@@ -1,11 +1,12 @@
 import random
-
-W, H = [100, 100] #Arbitrary Autoclave dimension
+####### Generate data to process
+W, H = [100, 100] # Arbitrary Autoclave dimension
 Autoclave_area = W*H
-#Define dimensions of jobs to be completed
+# Define dimensions of jobs to be completed
 Dimensions = []
-list = range(0,41) #number of jobs
-for i in list:
+list = range(0,41) # number of jobs
+for i in list: # Generate job geometries randomly, favourising a homogeneous distribution of large, average and small parts
+	       # The ratios with the width and height of the autoclave dimensions were taken arbitrarily
     if i < 10:
         W1 = random.randrange(int(2/3*W), W)
         H1 = random.randrange(1, int(1/2*H))
@@ -22,14 +23,13 @@ for i in list:
         W1 = random.randrange(1, int(1/2*W))
         H1 = random.randrange(1, int(1/2*H))
         Dimensions.append([W1, H1])
-
-random.shuffle(Dimensions)
+random.shuffle(Dimensions) # Ensure 
 Areas = [item[0]*item[1] for item in Dimensions]
-list_of_autoclaves = []
-#Sort list in decreasing order then apply the algorithm
-sorted_areas = sorted(Areas, reverse = True)
 
-#Define bin class
+####### Greedy algorithm
+# Sort list in decreasing order then apply the algorithm
+sorted_areas = sorted(Areas, reverse = True)
+# Define bin class
 class Bin:
 	def __init__(self):
 		self.list = []
@@ -49,20 +49,17 @@ class Bin:
 	def show(self):
 		return self.list
 
-def first_fit(areas, max_area):
-    # Returns list of bins with input items inside
+def first_fit(areas, max_area): # Returns list of bins with input items inside
     list_bins = []; list_bins.append(Bin())  # Add first empty bin to list
 
     for item in areas:
         bin_found = False # Go through bins and try to allocate
-
         for bin in list_bins:
             if bin.sum() + item <= max_area:
                 bin.addItem(item)
                 bin_found = True; break
-
         # If item not allocated in bins in list, create new bin and allocate it to it.
-        if bin_found== False:
+        if bin_found == False:
             newBin = Bin(); newBin.addItem(item); list_bins.append(newBin)
     
     areas = []
