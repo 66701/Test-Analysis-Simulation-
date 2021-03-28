cc5e8164dc6ee14016067474d1ccbfee1159dbef
66701 <74401121+66701@users.noreply.github.com>
2021-03-28 12:09:06 +0200

    Update Autoclave Packing.py

--- Autoclave Packing.py
+++ Autoclave Packing.py
@@ -2,11 +2,11 @@ import random
 ####### Generate data to process
 W, H = [1000, 1500] # LAB 115 Autoclave dimensions in [mm]
 Autoclave_area = W*H
-# Define dimensions of jobs to be completed
+#Define dimensions of jobs to be completed
 Dimensions = []
-list = range(0,41) # number of jobs
+list = range(0,41) #number of jobs
 for i in list: # Generate job geometries randomly, favourising a homogeneous distribution of large, average and small parts
-	       # The ratios with the width and height of the autoclave dimensions were taken arbitrarily
+    # The ratios with the width and height of the autoclave dimensions were taken arbitrarily
     if i < 10:
         W1 = random.randrange(int(2/3*W), W)
         H1 = random.randrange(1, int(1/2*H))
@@ -23,50 +23,49 @@ for i in list: # Generate job geometries randomly, favourising a homogeneous dis
         W1 = random.randrange(1, int(1/2*W))
         H1 = random.randrange(1, int(1/2*H))
         Dimensions.append([W1, H1])
-random.shuffle(Dimensions) # Ensure 
-Areas = [item[0]*item[1] for item in Dimensions]
 
-####### Greedy algorithm
-# Sort list in decreasing order then apply the algorithm
+random.shuffle(Dimensions)  # Ensure
+Areas = [item[0]*item[1] for item in Dimensions]
+list_of_autoclaves = []
+
+####### First Fit decreasing Greedy algorithm
+#Sort list in decreasing order then apply the algorithm
 sorted_areas = sorted(Areas, reverse = True)
-# Define bin class
+#Define bin class
 class Bin:
 	def __init__(self):
 		self.list = []
-
 	def addItem(self, item):
 		self.list.append(item)
-
 	def removeItem(self, item):
 		self.list.remove(item)
-
 	def sum(self):
 		total = 0
 		for elem in self.list:
 			total += elem
 		return total
-
 	def show(self):
 		return self.list
 
-def greedy(areas, max_area): # Will return the list of autoclave cycles with ply areas allocated to them
-    autoclaves = []; autoclaves.append(Bin())  # Add first empty bin to list
-
+def Greedy(areas, max_area): # Will return the list of autoclave cycles with ply areas allocated to them
+    list_bins = []
+    list_bins.append(Bin())  # Add first empty bin to list
     for item in areas:
         bin_found = False # Allocating the bins
-        for autoclave in autoclaves:
+        for bin in list_bins:
             if bin.sum() + item <= max_area:
                 bin.addItem(item)
-                bin_found = True; break
+                bin_found = True
+                break
         # If cannot locate the job in the bin make a new one
-        if bin_found == False:
-            newBin = Bin(); newBin.addItem(item); autoclave.append(newBin)
-    
+        if bin_found== False:
+            newBin = Bin()
+            newBin.addItem(item)
+            list_bins.append(newBin)
+    # Bins are transformed into lists of areas
     areas = []
     for bin in list_bins:
-        areas.append(bin.show())# Bins are transformed into lists of areas
+        areas.append(bin.show())
     return (areas)
 
-print(greedy(sorted_areas, Autoclave_area))
-
-#Note bin and autoclave are used interchangably 
+print(Greedy(sorted_areas, Autoclave_area))
